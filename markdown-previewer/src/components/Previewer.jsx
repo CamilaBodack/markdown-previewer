069448161c0bd2b91d580b3d755ea62a3de6f13d
CamilaBodack <camila.bodack@gmail.com>
2020-11-24 19:51:44 -0300

    test markdown previewer lib

--- markdown-previewer/src/components/Previewer.jsx
+++ markdown-previewer/src/components/Previewer.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Editor from './Editor'
-let marked = require("marked");
+import marked from 'marked'
 
 class Previewer extends Component {
     constructor(props) {
@@ -11,13 +11,8 @@ class Previewer extends Component {
         return (<div>
             <div id="content"></div>
             <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
-            <div dangerouslySetInnerHTML={{
-                    __html: marked('#Camila \n\nRendered by **marked**.')
-                }}/>
+            <div dangerouslySetInnerHTML={{__html: marked("#Welcome to Markdown Previewer\n\n##Here you can write beautiful markdown and preview the results\n\n Brief examples:\n\n1. `H1: #H1`\n\n2. `H2: ##H2`\n\n3. `<code>code</code>`\n\n4. Inline code `<addr>`\n\n**Need help with syntaxe ? access:** [Markdown Guide!](https://www.markdownguide.org/)")}} />
         </div>);
     }
-
-} {/*User Story #5: When my markdown previewer first loads, the default text in the #editor field should contain valid markdown that represents at least one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.*/
 }
-
 export default Previewer
