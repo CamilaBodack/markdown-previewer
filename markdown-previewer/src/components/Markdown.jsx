d2814d2873c11fee34046f37fd4dba008839a515
CamilaBodack <camila.bodack@gmail.com>
2020-11-26 16:46:07 -0300

    adjust tags in firstRender according to fcc test regex

--- markdown-previewer/src/components/Markdown.jsx
+++ markdown-previewer/src/components/Markdown.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import marked from 'marked'
 
 
-let firstRender = "# Preview:\n\n<hr></hr>\n\n## Here you can write beautiful markdown and preview the results\n\nUseful ways of showing information using markdown:\n\n+ Show code examples in instructions with tags code <code></code> `<code></code>` or inside ``.\n\n+ Use blockquotes with content inside `<blockquote></bloquote>` tags.\n\n+ If you like creating images or use emoticons you also can use cute emoticons ʕ•́ᴥ•̀ʔっ\n\n+ ... and images <img src='https://fsymbols.com/thumbs/54.png'  width='45' height='35' />\n\n**Need help with syntaxe ? access:** [Markdown Guide!](https://www.markdownguide.org/)"
+let firstRender = "# Preview:\n\n<hr></hr>\n\n## Here you can write beautiful markdown and preview the results\n\n### Useful ways of showing information using markdown:\n\n+ Show code examples in instructions with tags code\n\n         <pre><code>inside code tags</code></pre>\n\n<code>This is how a code is showed inside code tags</code>\n\n `npm install -g how is to be inside backticks`.\n\n+ Use blockquotes with content inside.\n\n  > Hello, i'm new blockquote :).\n\n+ If you like creating images or use emoticons you also can use cute emoticons ʕ•́ᴥ•̀ʔっ\n\n ![Blue Martini](https://icing.ca/wp-content/uploads/2016/04/icon-martini.png)\n\n**Need help with syntaxe ? access:** [Markdown Guide!](https://www.markdownguide.org/)"
 
 
 class Markdown extends Component {
@@ -26,13 +26,13 @@ class Markdown extends Component {
     render() {
         return ( < div className = "markdown-conteiner" >
             <h1>Welcome to Martini Markdown</h1>
-            <div id = "editor">
+            <div className = "editor">
             <div id = "content"></div>
             <script src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js" ></script>
             <textarea id = "editor" name = "editor" rows = "10" cols = "60"
-            form = "previewer" input type="text" value={this.state.value} onChange={this.handleChange}></textarea>
-            <form id = "previewer">
-            <div onChange = {this.handleChange} dangerouslySetInnerHTML = {
+            form = "preview" input type="text" value={this.state.value} onChange={this.handleChange}></textarea>
+            <form>
+            <div className = "preview" id= "preview" onChange = {this.handleChange} dangerouslySetInnerHTML = {
                 {
                     __html: marked(this.state.value)
                 }
