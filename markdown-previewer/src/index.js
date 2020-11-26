442b69972609afbefcbd71c190b1399732ced860
CamilaBodack <camila.bodack@gmail.com>
2020-11-26 13:41:45 -0300

    adjust style in editor and previewer

--- markdown-previewer/src/index.js
+++ markdown-previewer/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Previewer from './components/Markdown'
+import Markdown from './components/Markdown'
 import './index.css';
 
-ReactDOM.render(<Previewer />,document.getElementById('root'));
+ReactDOM.render(<Markdown />,document.getElementById('root'));
