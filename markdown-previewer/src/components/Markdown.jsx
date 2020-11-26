056ad738ff22481ee6168d150a3fad0bfcfaa972
CamilaBodack <camila.bodack@gmail.com>
2020-11-26 14:09:39 -0300

    add onChange in #editor and previewer

--- markdown-previewer/src/components/Markdown.jsx
+++ markdown-previewer/src/components/Markdown.jsx
@@ -1,9 +1,9 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from 'react';
 import marked from 'marked'
 
+
 let firstRender = "# Preview:\n\n<hr></hr>\n\n## Here you can write beautiful markdown and preview the results\n\nUseful ways of showing information using markdown:\n\n+ Show code examples in instructions with tags code <code></code> `<code></code>` or inside ``.\n\n+ Use blockquotes with content inside `<blockquote></bloquote>` tags.\n\n+ If you like creating images or use emoticons you also can use cute emoticons ʕ•́ᴥ•̀ʔっ\n\n+ ... and images <img src='https://fsymbols.com/thumbs/54.png'  width='45' height='35' />\n\n**Need help with syntaxe ? access:** [Markdown Guide!](https://www.markdownguide.org/)"
+
 
 class Markdown extends Component {
     constructor(props) {
@@ -12,7 +12,16 @@ class Markdown extends Component {
         this.state = {
             value: firstRender
         }
+
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange(event){
+        this.setState(
+            {value: event.target.value}
+        );
+      }
+
 
     render() {
         return ( < div className = "markdown-conteiner" >
@@ -20,18 +29,18 @@ class Markdown extends Component {
             <div id = "editor">
             <div id = "content"></div>
             <script src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js" ></script>
-            <textarea id = "editor"
-            name = "editor"
-            rows = "10"
-            cols = "60"
-            form = "previewer" input type="text" value={this.state.value}></textarea><form id = "previewer">
-            <div dangerouslySetInnerHTML = {
+            <textarea id = "editor" name = "editor" rows = "10" cols = "60"
+            form = "previewer" input type="text" value={this.state.value} onChange={this.handleChange}></textarea>
+            <form id = "previewer">
+            <div onChange = {this.handleChange} dangerouslySetInnerHTML = {
                 {
-                    __html: marked(firstRender)
+                    __html: marked(this.state.value)
                 }
             }/></form ></div>
             <footer>[ Created by Camila Bodack 2020 ]</footer>
             </div>);
         }
     }
-    export default Markdown
+
+
+export default Markdown
